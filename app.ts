90f43323f854ed83cc471703b461772b7ce86095
Tri Denda <tridenda.nk@gmail.com>
2023-02-13 21:52:38 +0700

    primitive types: number, string, and boolean

--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("Your code goes here");
+function add(n1:number, n2:number) {
+  return n1 + n2;
+}
+
+const num1 = 23;
+const num2 = 2.8;
+const result = add(num1, num2);
+
+console.log(result);
